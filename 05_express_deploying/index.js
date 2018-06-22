7352c378d43974548c7109b70373b7faa6365012
Viktor Pancheluga <v.pancheluga@adbakers.com.ua>
2018-06-22 14:03:24 +0300

    Setup start script in heroku port

--- 05_express_deploying/index.js
+++ 05_express_deploying/index.js
@@ -1,10 +1,30 @@
 const express = require('express');
 const hbs = require('hbs');
+const fs = require('fs');
+
+const port = process.env.PORT || 3000;
 
 let app = express();
 
 hbs.registerPartials(__dirname + '/views/partials');
 app.set('view engine', 'hbs');
+
+app.use((req, res, next) => {
+    let now = new Date().toString();
+    var log = `${now}: ${req.method} ${req.url}`;
+    console.log(log);
+    fs.appendFile('server.log', log + '\n', (err) => {
+        if (err) {
+            console.log('Unable to append to server.log');
+        }
+    });
+    next();
+});
+
+// app.use((req, res, next) => {
+//     res.render('maintenance.hbs');
+// });
+
 app.use(express.static(__dirname + '/public'));
 
 hbs.registerHelper('getCurrentYear', () => {
@@ -37,6 +57,6 @@ app.get('/bad', (req, res) => {
 
 
 
-app.listen(3001, () => {
-    console.log('Server is up on port 3001');
+app.listen(port, () => {
+    console.log(`Server is up on port ${port}`);
 });

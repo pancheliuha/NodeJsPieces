deac627f2d88ad22924dd7d9c69dbf80eb6a0ba6
Viktor Pancheluga <v.pancheluga@adbakers.com.ua>
2018-06-23 19:03:44 +0300

    add users initial model

--- node-todo-api/server/models/user.js
+++ node-todo-api/server/models/user.js
@@ -1,12 +1,42 @@
 const mongoose = require('mongoose');
+const validator = require('validator');
+
+// {
+//     email: 'vutya2806@gmail.com',
+//     password: 'secret123',
+//     tokens: [{
+//         access: 'auth',
+//         token: 'fsagasgasgswgsgaslkgk'
+//     }]
+// }
 
 let User = mongoose.model('User', {
     email: {
         type: String,
         required: true,
         minlength: 1,
-        trim: true
-    }
+        trim: true,
+        unique: true,
+        validate: {
+            validator: validator.isEmail,
+            message: '{VALUE} is not a valid email'
+        }
+    },
+    password: {
+        type: String,
+        required: true,
+        minlength: 6
+    },
+    tokens: [{
+        access: {
+            type: String,
+            required: true
+        },
+        token: {
+            type: String,
+            required: true
+        }
+    }]
 });
 
 module.exports = {User};

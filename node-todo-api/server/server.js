f58786e92fa48b8db9f812f6b54bf2ffc7f0a14b
Viktor Pancheluga <v.pancheluga@adbakers.com.ua>
2018-06-23 13:16:37 +0300

    GET /todos/:id

--- node-todo-api/server/server.js
+++ node-todo-api/server/server.js
@@ -1,5 +1,7 @@
 let express = require('express');
 let bodyParser = require('body-parser');
+const {ObjectID} = require('mongodb');
+
 
 let {mongoose} = require('./db/mongoose');
 let {Todo} = require('./models/todo');
@@ -31,6 +33,27 @@ app.get('/todos', (req, res) => {
         .catch(e => {
             res.status(400).send(e)
         })
+});
+
+//GET /todos/1234123
+app.get('/todos/:id', (req, res) => {
+    let id = req.params.id;
+
+    if (!ObjectID.isValid(id)) {
+        return res.status(404).send();
+    }
+
+    Todo.findById(id)
+        .then(todo => {
+            if (!todo) {
+                return res.status(404).send();
+            }
+
+            res.send({todo});
+        })
+        .catch(e => {
+            res.status(400).send();
+        });
 });
 
 app.listen(3000, () => {
